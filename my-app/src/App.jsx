0e0071686984746ac362a0fd0989ad1f378f0ff4
SaumyarupGuha <guhasaumyarup100@gmail.com>
2025-09-07 01:30:41 +0530

    Done till Search Feature

---
+++ my-app/src/App.jsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react"
+import Search from "./Components/Search"
+import MovieCard from "./components/MovieCard";
+import { useDebounce } from "use-debounce";
+
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
+const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const [movieList, setMovieList] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  // const[debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+
+  // useDebounce(()=>setDebouncedSearchTerm(searchTerm),500, [searchTerm])
+
+  const[debouncedSearchTerm] = useDebounce(searchTerm, 500);
+
+  const fetchMovies = async (query='') => {
+
+    setIsLoading(true);
+    setErrorMessage('');
+
+    try {
+
+      const endpoint = searchTerm ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok){
+        throw new Error('Failed to fetch movies');
+      }
+
+      const data = await response.json();
+
+      if(data.response === 'False'){
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
+
+      }
+      setMovieList(data.results || []);
+
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Failed to fetch movies. Please try again later.');
+    }  finally{
+      setIsLoading(false);
+    }
+  }
+
+  useEffect (() => {
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+  
+
+
+  return (
+    <main>
+      <div className="pattern">
+        <img src='./hero-bg.png' alt='hero-bg' className="absolute inset-0 w-full h-full object-cover -z-10"/>
+
+        <div className="wrapper">
+          <header>
+            <img src='./hero.png' alt='hero'/>
+            <h1> Find Your Favourite <span className="text-gradient">Movies</span></h1>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          </header>
+          
+          <section className="all-movies">
+            <h2>All Movies</h2>
+
+            {isLoading ? (
+              <p className="text-white">Loading....</p>
+            ) : errorMessage ? (
+              <p className="text-red-500">{errorMessage}</p>
+            ) : (
+              <ul>
+                {movieList.map((movie) => (
+                  <MovieCard key={movie.id} movie={movie} />
+                ))}
+              </ul>
+            )}
+          </section>
+        </div>
+      </div>
+     
+    </ main>
+  )
+}
+
+export default App
---
+++ my-app/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+
+
+const HomePage = () =>{
+    return(
+        <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
+        <h1 className="text-5xl text-blue-800 font-bold">MovieMania</h1>
+        <div className="flex-1 p-4 flex justify-center items-center">
+        <div className="border-red-400 m-4 border-8 w-1/3 h-full">Hello</div>
+        <div className="border-red-400 m-4 border-8 w-1/3 h-full">Hello</div>
+        <div className="border-red-400 m-4 border-8 w-1/3 h-full">Hello</div>
+        </div>
+        </div>
+    )
+}
+
+export default HomePage
